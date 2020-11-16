e13784669b259bc5ea716bccc3ffae015136fa9b
kraell <kraell@umich.edu>
2020-11-15 19:01:58 -0500

    Starter files + .gitignore

---
+++ script/main.js
@@ -0,0 +1,18 @@
+/*
+ * University of Michigan
+ * EECS 493, Fall 2020
+ *
+ * main.js
+ * eecs493-final-project
+ *
+ * Copyright © 2020 Marina Caporuscio, Juliet Carpenter, Keegan Elliott, Milan Gupta. All rights reserved.
+ */
+
+// TODO: Create components here above the Vue app
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hidey Ho, Neighbor'
+    }
+})
